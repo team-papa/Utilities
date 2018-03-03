096d25fc0ef66db6667cc20a01560a698b5ee193
Unknown <cjjensen01@aol.com>
2018-03-03 17:06:23 +0000

    Implement combining of images

--- src/utils.java
+++ src/utils.java
@@ -1,7 +1,10 @@
 import javax.imageio.ImageIO;
 import javax.imageio.ImageReadParam;
 import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.*;
 
@@ -11,25 +14,39 @@ public class utils {
      * Exemplar main method
      */
     public static void main(String[] args) {
-        String workingFolder = System.getProperty("user.dir");
+//        String workingFolder = System.getProperty("user.dir");
+//
+//        String populated = splitImages("Final Maps",
+//                workingFolder, "Map%d_%d.png", "TileMapsFoldered\\TileMap%d\\%d\\%d\\%d.png", 2, 3, 7, 256);
+//        System.out.print("Images Split Correctly");
+//        try {
+//            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(new File("PopulatedFiles")));
+//            bufferedOutputStream.write(populated.getBytes("UTF-8"));
+//            bufferedOutputStream.close();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (UnsupportedEncodingException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
-        final int maxFloors = 3;
-        final int maxZoom = 8;
-        final int tileWidth = 256;
-
-        String populated = splitImages(args.length > 0 ? args[0] : workingFolder, workingFolder, "Map", "TileMapsFoldered\\TileMap", maxFloors, maxZoom, tileWidth);
-        System.out.print("Images Split Correctly");
+//        genBlankTile("TileMapsNewFoldered", 256, Color.WHITE);
         try {
-            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(new File("PopulatedFiles")));
-            bufferedOutputStream.write(populated.getBytes("UTF-8"));
-            bufferedOutputStream.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            BufferedImage A =loadImageFromFile("TileMap2/1/0/0.png");
+            BufferedImage B =loadImageFromFile("TileMap2/1/1/0.png");
+            BufferedImage C =loadImageFromFile("TileMap2/1/0/1.png");
+            BufferedImage D =loadImageFromFile("TileMap2/1/1/1.png");
+
+            BufferedImage result = combineImageImage(A,B,C,D);
+            ImageIO.write(result, "png", new File("combined.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static BufferedImage loadImageFromFile(String path) throws IOException {
+        return ImageIO.read(new File(path));
     }
 
     /**
@@ -39,20 +56,22 @@ public class utils {
      * @param destPath       the resultant path of the files
      * @param baseSrcString  the base format of the input files taking an two integers floor and zoom
      * @param baseDestString the base format of the output files
-     * @param maxFloors      the maximum number of floors
-     * @param maxZoom        the maximum zoom level
+     * @param minFloor      the minimum floor number
+     * @param maxFloorInc   the maximum number of floors
+     * @param maxZoomInc        the maximum zoom level
      * @param tileWidth      the width of each tile
      * @return returns a String containing all populated tiles in the format "(<floor>,<zoom>,<x>,<y>);"
      */
-    private static String splitImages(String srcPath, String destPath, String baseSrcString, String baseDestString, int maxFloors, int maxZoom, int tileWidth) {
+    private static String splitImages(String srcPath, String destPath, String baseSrcString, String baseDestString, int minFloor, int maxFloorInc, int maxZoomInc, int tileWidth) {
         StringBuilder res = new StringBuilder();
-        for (int f = 2; f < maxFloors; f++) {
-            for (int z = 0; z < maxZoom; z++) {
+        for (int f = minFloor; f <= maxFloorInc; f++) {
+            for (int z = 0; z <= maxZoomInc; z++) {
                 try {
                     //load in each row of tiles into memory before splitting to increase efficiency of memory access
-                    File file = new File(String.format(srcPath + "\\" + baseSrcString, (f + 1), z));
+                    File file = new File(String.format(srcPath + "\\" + baseSrcString, f, z));
                     ImageReader imageReader = ImageIO.getImageReadersBySuffix("png").next();
-                    imageReader.setInput(ImageIO.createImageInputStream(file), false);
+                    ImageInputStream imageInputStream = ImageIO.createImageInputStream(new FileInputStream(file));
+                    imageReader.setInput(imageInputStream, false);
                     for (int y = 0; y < Math.pow(2, z); y++) {
                         Rectangle rowRect = new Rectangle(0, y * tileWidth, imageReader.getWidth(0), tileWidth);
                         ImageReadParam paramRow = imageReader.getDefaultReadParam();
@@ -61,7 +80,8 @@ public class utils {
                         for (int x = 0; x < Math.pow(2, z); x++) {
                             BufferedImage img = xRow.getSubimage(x * tileWidth, 0, tileWidth, tileWidth);
                             if (!allWhite(img)) {
-                                File destFile = new File(destPath + "\\" + baseDestString + (f + 1) + "\\" + z + "\\" + x + "\\" + y + ".png");
+//                                File destFile = new File(destPath + "\\" + baseDestString + (f + 1) + "\\" + z + "\\" + x + "\\" + y + ".png");
+                                File destFile = new File(destPath + "\\" + String.format(baseDestString, f, z, x, y));
                                 destFile.getParentFile().mkdirs();
                                 ImageIO.write(img, "png", destFile);
 
@@ -69,15 +89,14 @@ public class utils {
                                 res.append(String.format("(%d,%d,%d,%d);", f, z, x, y));
                             }
                         }
-                        System.out.print("\r" + (y + 1));
+                        System.out.print(String.format("\r Splitting floor %d: at line %d/%d", f,  (y + 1), (int)Math.pow(2, z)));
                     }
-                    System.out.print("\n");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
-
+        System.out.print("\n");
         return res.toString();
     }
 
@@ -94,5 +113,60 @@ public class utils {
             }
         }
         return true;
+    }
+
+    public static void genBlankTile(String destPath, int TileWidth, Color blank){
+        BufferedImage bfi = new BufferedImage(TileWidth, TileWidth, BufferedImage.TYPE_INT_ARGB);
+        for(int i = 0; i < TileWidth; i++){
+            for(int j = 0; j < TileWidth; j++){
+                bfi.setRGB(i, j, blank.getRGB());
+            }
+        }
+
+        File destFile = new File(destPath + "\\Blank.png");
+        destFile.getParentFile().mkdirs();
+        try {
+            ImageIO.write(bfi, "png", destFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Combines 4 images of resolution XxY and into one XxY image
+     * All input images must be of the same resolution
+     * @param A Top Left
+     * @param B Top Right
+     * @param C Bottom Left
+     * @param D Bottom Right
+     * @return
+     */
+    public static BufferedImage combineImageImage(BufferedImage A, BufferedImage B, BufferedImage C, BufferedImage D){
+//        BufferedImage res = new BufferedImage(A.getWidth(), B.getHeight(), BufferedImage.TYPE_INT_ARGB);
+//        for(int i = 0; i < A.getWidth(); i++){
+//            for(int j = 0; j < A.getHeight(); j++){
+//                int rgb1 = getPixel(A,B,C,D,i*2,    j*2);
+//                int rgb2 = getPixel(A,B,C,D,i*2+1,  j*2);
+//                int rgb3 = getPixel(A,B,C,D,i*2,    j*2+1);
+//                int rgb4 = getPixel(A,B,C,D,i*2+1,  j*2+1);
+//
+//                res.setRGB(i,j, averageRGB(rgb1, rgb2, rgb3, rgb4));
+//            }
+//        }
+//
+//        return res;
+
+        BufferedImage result = new BufferedImage(A.getWidth(), A.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+        AffineTransform scale = AffineTransform.getScaleInstance(0.5, 0.5);
+        AffineTransformOp scaleOp = new AffineTransformOp(scale, AffineTransformOp.TYPE_BICUBIC);
+
+        Graphics2D gfx = result.createGraphics();
+        gfx.drawImage(A, scaleOp, 0, 0);
+        gfx.drawImage(B, scaleOp, A.getWidth() / 2, 0);
+        gfx.drawImage(C, scaleOp, 0, A.getHeight() / 2);
+        gfx.drawImage(D, scaleOp, A.getWidth() / 2, A.getHeight() / 2);
+        gfx.dispose();
+        return result;
     }
 }
